171a414280f4f13f2e98f57c2eb9204496cb6971
Andy <andy@wireworldmedia.co.uk>
2025-09-13 21:13:34 +0100

    Simplify

--- refresh_date_navs.rb
+++ refresh_date_navs.rb
@@ -3,7 +3,6 @@
 require_relative 'date_nav'
 
 JOURNAL_PATH = '/Users/andy/Dropbox/Obsidian/Personal/journal'
-UNWANTED_NAV = "```dataviewjs\nconst {DateNav} = customJS\nDateNav.nav(dv)\n```\n"
 
 # List of paths
 available_date_paths = Dir.glob("#{JOURNAL_PATH}/????-??-??.md").sort
@@ -15,37 +14,22 @@ rescue ArgumentError
   nil
 end.compact
 
-def save(lines, path)
-  File.open(path, 'w') { |f| f.write lines.join }
-  # puts "\n------> #{path}\n#{lines.join}"
+def save(contents, path)
+  File.open(path, 'w') { |f| f.write contents }
+  # puts "\n------> #{path}\n#{contents}"
 end
 
 # For each file
 available_date_paths.each do |path|
+  latest_nav = DateNav.new(Date.parse(path), available_dates).write
   contents = File.read(path)
-  cleaned_contents = contents.sub(UNWANTED_NAV, '')
-  lines = cleaned_contents.split("\n").map { |line| "#{line}\n" }
+  updated_contents = contents.sub(/^.*[≪≫].*$/, latest_nav)
 
-  unless lines[0] =~ /^#/
-    puts "#{path}: file does not start with a #, skipping"
-    next
-  end
-
-  existing_nav = lines[1]&.chomp # Always 2nd line
-  new_nav = DateNav.new(Date.parse(path), available_dates).write
-
-  if existing_nav =~ /[<>≪≫]/
-    if existing_nav == new_nav
-      print '.'
-    else
-      print 'u'
-      lines[1] = "#{new_nav}\n"
-      save(lines, path)
-    end
+  if contents == updated_contents
+    print '.'
   else
-    print 'i'
-    lines[0] = "#{lines[0]}#{new_nav}\n\n"
-    save(lines, path)
+    print 'u'
+    save(updated_contents, path)
   end
 end
 
